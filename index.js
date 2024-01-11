9bd442a6270fe0f6f5acff0c59ab132a8955b739
andsana <sanadex90@gmail.com>
2024-01-11 18:47:15 +0900

    Exercise 2. Сreate an API for encrypting and decrypting text based on the Vigenère cipher.

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const Vigenere = require('caesar-salad').Vigenere;
 const app = express();
 const port = 8000;
 
 app.get('/:message', (req, res) => {
     res.send(req.params.message);
+});
+
+app.get('/encode/:message', (req, res) => {
+    const encryptedText = Vigenere.Cipher('password').crypt(req.params.message);
+    res.send(encryptedText);
+});
+app.get('/decode/:message', (req, res) => {
+    const decryptedText = Vigenere.Decipher('password').crypt(req.params.message);
+    res.send(decryptedText);
 });
 
 app.listen(port, () => {
